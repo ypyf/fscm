2dde0d93d317fe92f69320fe46acead095149b96
Yan Feng <t34@qq.com>
2014-09-01 10:34:32 +0800

    Add test scope.scm

---
+++ test/scope.scm
@@ -0,0 +1,17 @@
+(define (foo)
+    (call/cc
+         (lambda (return)
+                (define (f) (return "return from foo from inside proc"))
+                     (f) ; control leaves foo here
+                          (return "return from foo"))))
+
+(define (bar)
+    (call/cc
+         (lambda (return)
+                (define (f) (call/cc (lambda (return) (return "return from lambda"))))
+                     (f) ; control does not leave bar here
+                          (return "return from bar"))))
+
+(display (foo)) ; prints "return from foo from inside proc"
+(newline)
+(display (bar)) ; prints "return from bar"
