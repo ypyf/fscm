9695d5a45829bcb467583904f09eda5c83be91fe
yf <t34@qq.com>
2019-02-03 01:17:54 +0800

    Fix stdlib.scm

--- src/stdlib.scm
+++ src/stdlib.scm
@@ -12,7 +12,7 @@
 (define (id obj) obj)
 (define (flip func) (lambda (arg1 arg2) (func arg2 arg1)))
 (define (curry func arg1) (lambda (arg) (apply func (cons arg1 (list arg)))))
-(define (compose f g) (lambda (arg) (f (apply g args))))
+(define (compose f g) (lambda (args) (f (apply g args))))
 
 
 (define zero? (curry = 0))
